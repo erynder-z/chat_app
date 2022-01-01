426f8b055835f212cb0e8c5936aa2eecae02ca19
eryner-z <stfn.bgr@gmail.com>
2022-01-01 18:06:30 +0100

    fix cors error on live server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 //create a server on port 3000
-const io = require("socket.io")(3000);
+const io = require("socket.io")(3000,{
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"]
+  },
+});
 
 //assign socket to user on connection to server
 io.on("connection", socket => {
-    socket.emit("chat-message", "hello world!")
-})
+    socket.emit("chat-message", "hello world!");
+});
