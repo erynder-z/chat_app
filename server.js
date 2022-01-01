7fff2ba393d051abb158011e85639ae4cea97357
eryner-z <stfn.bgr@gmail.com>
2022-01-01 17:17:20 +0100

    assign socket upon connection

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 //create a server on port 3000
 const io = require("socket.io")(3000);
+
+//assign socket to user on connection to server
+io.on("connection", socket => {
+    socket.emit("chat-message", "hello world!")
+})
