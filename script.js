c673fda32892a3011a202733be683a82379e9cf3
eryner-z <stfn.bgr@gmail.com>
2022-01-02 11:45:13 +0100

    message send functionality

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //get the server hosted in server.js
 const socket = io("http://localhost:3000");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
-// get data emitted by server
+//get data emitted by server
 socket.on("chat-message", data => {
     console.log(data);
-})
+});
+
+//add eventListener and prevent default page refresh on button press
+messageForm.addEventListener("submit", event => {
+    event.preventDefault();
+    //get value from message input and send that message to the server
+    const message = messageInput.value;
+    socket.emit("send-chat-message", message);
+    messageInput.value = ""
+});
