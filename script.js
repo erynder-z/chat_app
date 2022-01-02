7ee054ff229054e6d1aecc8413946fb1b6e2b9f7
eryner-z <stfn.bgr@gmail.com>
2022-01-02 14:39:24 +0100

    change DOM to display message

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //get the server hosted in server.js
 const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 //get data emitted by server
 socket.on("chat-message", data => {
-    console.log(data);
+    appendMessage(data);
 });
 
 //add eventListener and prevent default page refresh on button press
@@ -16,3 +17,10 @@ messageForm.addEventListener("submit", event => {
     socket.emit("send-chat-message", message);
     messageInput.value = ""
 });
+
+//display in clients browser
+function appendMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
