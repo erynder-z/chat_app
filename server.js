ccdd3acb79974b7856b9b57923b0cdbd0ba99833
eryner-z <stfn.bgr@gmail.com>
2022-01-04 10:41:32 +0100

    allow all connections in CORS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const runServer = (() => {
   //create a server on port 3000
   const io = require("socket.io")(3000, {
     cors: {
-      origin: "http://127.0.0.1:5500", // replace with your server IP
+      origin: "*", //needs to be proberly configured IRL
     },
   });
 
